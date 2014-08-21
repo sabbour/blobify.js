06648e505a69d6141e74168cdc2b53b4aec34d53
Ahmed Sabbour <sabb0ur@gmail.com>
2014-08-21 11:49:52 +0300

    Further implementing the upload mechanism

--- Uploadify/Scripts/blobify/blobify.plugin.js
+++ Uploadify/Scripts/blobify/blobify.plugin.js
@@ -57,12 +57,13 @@
                 input.parent().find("a[role='browse-button']").click(function () { $(this).closest(".blobify-container").find("input.blobify").click() });
 
                 // Attach click handler to Upload button
-                input.parent().find("a[role='upload-button']").click($.beginUpload);
+                input.parent().find("a[role='upload-button']").click($.initializeUpload);
 
                 // Attach the change handler on the input field
                 input.change(function () {
                     // Set the filename (if found on the template)
-                    input.parent().find("span[role='filename']").html(input.val());
+                    var files = input[0].files; // FileList object. input[0] will get us the DOM object from the jQuery object
+                    input.parent().find("span[role='filename']").html(files[0].name);
 
                     // Enable the upload button
                     input.parent().find("a[role='upload-button']").removeClass("disabled");
@@ -72,12 +73,13 @@
         });
     };
 
-    $.beginUpload = function (e) {
+    $.initializeUpload = function (e) {
         var parent = $(e.target).closest(".blobify-container");
         var input = parent.find("input.blobify");
         var targetContainer = input.data("container");
 
-        otalUploadedBlocks = 0;
+
+        totalUploadedBlocks = 0;
         totalNumberOfBlocks = 0;
         // assert the browser support html5
         if (!(window.File && window.Blob && window.FormData)) {
@@ -85,23 +87,34 @@
             return;
         }
 
+        var files = input[0].files; // FileList object. input[0] will get us the DOM object from the jQuery object
+        var fileName = escape(files[0].name);
+
         $.disableControlsForUpload(parent);
 
-        // Retrieve file name from file, or generate a new one or rely on the one from the server
-        $.obtainSAS(targetContainer,"some-blob-name-from-the-file");
+        // Retrieve an upload destination that will include a SAS, then begin upload
+        $.obtainSAS(targetContainer,fileName,parent,$.beginUpload);
     }
 
-    $.obtainSAS = function (containerName,blobName) {
+    $.obtainSAS = function (containerName,blobName,parent,callback) {
         $.get(settings.sasEndpoint + "?containerName=" + containerName + "&blobName=" + blobName, function (response) {
             if (!response.sas) {
                 alert("There was an error obtaining the token required to upload.");
             }
             else {
-                // save response.url in the hidden field, this is the final destination
-                // use response.upload_destination as the post destination of the upload, it contains the SAS
-                alert(response.upload_destination);
+                callback(parent,response);
             }
         });
+    }
+
+    $.beginUpload = function(parent,sasResponse) {
+        var input = parent.find("input.blobify");
+
+        // We will support only a single file upload
+        var file = input[0].files[0];
+        if (file != undefined) {
+
+        }
     }
 
     $.disableControlsForUpload = function (parent) {
